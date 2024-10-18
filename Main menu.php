8eb5e6fd5c527b87c81295a1c0a16848ce3c0480
Taapopi Ndeshipanda <taapopindeshipanda@gmail.com>
2024-10-19 00:14:14 +0200

    doctor homepage

---
+++ Main menu.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Electronic Health Information System</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        text-align: center;
+        background-color: #f4f4f4;
+      }
+
+      h1 {
+        color: #333;
+        margin-top: 50px;
+      }
+
+      .button-container {
+        margin-top: 51px;
+      }
+
+      .button {
+        display: inline-block;
+        padding: 15px 30px;
+        margin: 10px;
+        font-size: 18px;
+        color: #fff;
+        background-color: #89cff0;
+        border: none;
+        border-radius: 8px;
+        text-decoration: none;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+      }
+
+      .button:hover {
+        background-color: #66a3d2;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Electronic Health Information System</h1>
+
+    <div class="button-container">
+      <a href="./Admin/Adminview.html" class="button">Admin View</a>
+      <a href="./Patient/Patientview.html" class="button">Patient View</a>
+      <a href="./Doctor/DoctorHomepage.php" class="button">Doctor View</a>
+    </div>
+  </body>
+</html>
