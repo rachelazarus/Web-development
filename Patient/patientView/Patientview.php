ea87ee6308dc981c5fc6d0a42f1216234259d2d0
Thomas shikalepo deon <thomasshikalepo@gmail.com>
2024-10-25 16:58:18 +0200

    continued with the patient view

--- Patient/patientView/Patientview.php
+++ Patient/patientView/Patientview.php
@@ -1,23 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="icon" href="../favicon/medical-check.png" type="image/x-icon">
-    <link rel="stylesheet" href="./style.css">
-    <title>Patient View</title>
-
-
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://kit.fontawesome.com/c1df782baf.js"></script>
+  <link rel="icon" href="../favicon/medical-check.png" type="image/x-icon">
+  <link rel="stylesheet" href="./style.css">
+  <title>Patient View</title>
 </head>
 
 <body>
-    <header>
+  <header>
     <div class="logo">
-      <img src="../images/logo2.png" alt="Hostpital logo" />
+      <img src="../images/logo2.png" alt="Hospital logo" />
     </div>
-
     <nav class="navbar">
       <ul class="nav-links">
         <li><a href="#home">Home</a></li>
@@ -25,12 +21,30 @@
         <li><a href="#Contact">Contact</a></li>
       </ul>
     </nav>
-
-    <div class="right-icons">
-      <div id="menu-bars" class="fas fa-bars"></div>
-      <div class="btn" id="#"></div>
+    <div id="menu-bars" class="fas fa-bars"></div>
+    <div class="profile_picture">
+      <img src="../images/user (1).png" alt="User Profile">
     </div>
-    </header>
-    <script src="./script.js"></script>
+  </header>
+
+  <main>
+    <div class="Profile-container">
+      <div class="profile-picture">
+        <img src="../images/pexels-laker-5792641.jpg" alt="Profile picture">
+      </div>
+
+      <div>
+        <div class="contact-container">
+          <p><img src="../favicon/email.png" alt="Email Icon"> Email: </p>
+          <p><img src="../favicon/telephone (1).png" alt="Phone Icon"> Phone Number: </p>
+        </div>
+        <div class="choose">
+          <button type="submit" class="getRecord">Get Records</button>
+          <button type="submit" class="Appointment">Make Appointment</button>
+        </div>
+      </div>
+    </div>
+  </main>
+  <script src="./script.js"></script>
 </body>
 </html>
